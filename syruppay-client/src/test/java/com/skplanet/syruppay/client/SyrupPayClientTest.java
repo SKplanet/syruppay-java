50975ee78e949dedae4537ca83c582cc29b5d9dc
byplacebo <placebo2me@gmail.com>
2016-04-01 14:51:14 +0900

    자바 SDK 기준 Client 요청 코드 샘플 추가

--- syruppay-client/src/test/java/com/skplanet/syruppay/client/SyrupPayClientTest.java
+++ syruppay-client/src/test/java/com/skplanet/syruppay/client/SyrupPayClientTest.java
@@ -24,6 +24,10 @@
 
 package com.skplanet.syruppay.client;
 
+import com.skplanet.jose.Jose;
+import com.skplanet.jose.JoseBuilders;
+import com.skplanet.jose.JoseHeader;
+import com.skplanet.jose.jwa.Jwa;
 import com.skplanet.syruppay.client.event.ApproveEvent;
 import com.skplanet.syruppay.client.event.GetSsoCredentialEvent;
 import com.skplanet.syruppay.client.mocks.ApproveMocks;
@@ -35,9 +39,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -154,19 +160,56 @@ public class SyrupPayClientTest {
 
     @Test
     public void testGet_Sso_WITH_BASIC_AUTHENTICATION() throws IOException {
-        URL url = new URL("http://ip:port/login");
-        String encoding = Base64.encodeBase64String("id:password".getBytes());
+        URL url = new URL("https://devqapay.syrup.co.kr/v1/api-basic/merchants/sktmall_s002/sso-credentials/create");
+
+        String encoding = org.apache.commons.codec.binary.Base64.encodeBase64String("sktmall_s002:W9n7yLQrpt2Sm1zpqFpxr9k95BWNRXxs".getBytes());
 
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setDoOutput(true);
         connection.setRequestProperty("Authorization", "Basic " + encoding);
-        InputStream content = connection.getInputStream();
-        BufferedReader in =
-                new BufferedReader(new InputStreamReader(content));
-        String line;
-        while ((line = in.readLine()) != null) {
-            System.out.println(line);
+        connection.setRequestProperty("Accept", "application/jose");
+        connection.setRequestProperty("Content-type", "application/jose");
+
+        JoseHeader h = new JoseHeader(Jwa.A128KW, Jwa.A128CBC_HS256, "sktmall_s001");
+        String request = new Jose().configuration(
+                JoseBuilders.JsonEncryptionCompactSerializationBuilder()
+                        .header(h)
+                        .payload("{\n" +
+                                "    \"ssoIdentifier\": {\n" +
+                                "        \"mctUserId\" : \"15644623\"\n" +
+                                "    }\n" +
+                                "}")
+                        .key("G3aIW7hYmlTjag3FDc63OGLNWwvagVUU")
+        ).serialization();
+
+        OutputStream out = connection.getOutputStream();
+        out.write(request.getBytes());
+        out.close();
+
+        try {
+            InputStream content = connection.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(content));
+            String line;
+            StringBuilder buf = new StringBuilder();
+            while ((line = in.readLine()) != null) {
+                buf.append(line);
+            }
+
+            String response = new Jose().configuration(
+                    JoseBuilders.compactDeserializationBuilder()
+                            .serializedSource(buf.toString())
+                            .key("G3aIW7hYmlTjag3FDc63OGLNWwvagVUU")
+            ).deserialization();
+
+            System.out.println(response);
+
+            in.close();
+        } catch(FileNotFoundException fe) {
+            // 사용자 없음
+            System.out.println("User Not Found");
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
 

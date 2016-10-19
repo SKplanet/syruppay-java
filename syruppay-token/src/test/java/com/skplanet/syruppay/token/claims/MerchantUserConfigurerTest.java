ca46b2163ce2449b64b60bf54c85473d591d3cbf
byplacebo <placebo2me@gmail.com>
2016-10-19 10:32:34 +0900

    #65 테스트 코드 추가

---
+++ syruppay-token/src/test/java/com/skplanet/syruppay/token/claims/MerchantUserConfigurerTest.java
@@ -0,0 +1,30 @@
+package com.skplanet.syruppay.token.claims;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author 1002225
+ * @since 2016. 10. 19..
+ */
+public class MerchantUserConfigurerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSsoPolicy_equasl_with_null() {
+        assertThat(MerchantUserConfigurer.SsoPolicy.NOT_APPLICABLE.equals(null), is(false));
+    }
+
+}
